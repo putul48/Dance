cbd7790dcf72d7c550865f5adada07f01d19db19
putul48 <55074775+putul48@users.noreply.github.com>
2019-10-07 11:48:37 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+var config = {
+    apiKey: "AIzaSyBrRzWkRST39XTkNrRapSWSwCn5moglgUs",
+    authDomain: "danceputul48.firebaseapp.com",
+    databaseURL: "https://danceputul48.firebaseio.com",
+    projectId: "danceputul48",
+    storageBucket: "danceputul48.appspot.com",
+    messagingSenderId: "732151917610",
+    appId: "1:732151917610:web:eeea255c584b224a39e753",
+    measurementId: "G-DM0MF417E6"
+  };
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("logout_button").style.display = "block";
+    document.getElementById("login_button").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    window.alert("Welcome");
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("logout_button").style.display = "none";
+    document.getElementById("login_button").style.display = "block";
+
+  }
+});
+
+function logout(){
+  firebase.auth().signOut();
+  document.location.href="./index.html";
+}
